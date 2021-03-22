c340701f365a0531bc4a4740d47fb10c50ea8d60
Fatih Mehmet ARSLAN <iletisim@fmarslan.com>
2021-03-22 17:59:41 +0000

    solr connection

--- src/ConfigEditor.tsx
+++ src/ConfigEditor.tsx
@@ -1,7 +1,7 @@
 import React, { ChangeEvent, PureComponent } from 'react';
 import { LegacyForms } from '@grafana/ui';
 import { DataSourcePluginOptionsEditorProps } from '@grafana/data';
-import { MyDataSourceOptions, MySecureJsonData } from './types';
+import { MyDataSourceOptions } from './types';
 
 const { SecretFormField, FormField } = LegacyForms;
 
@@ -19,63 +19,30 @@ export class ConfigEditor extends PureComponent<Props, State> {
     onOptionsChange({ ...options, jsonData });
   };
 
-  // Secure field (only sent to the backend)
-  onAPIKeyChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const { onOptionsChange, options } = this.props;
-    onOptionsChange({
-      ...options,
-      secureJsonData: {
-        apiKey: event.target.value,
-      },
-    });
-  };
-
-  onResetAPIKey = () => {
-    const { onOptionsChange, options } = this.props;
-    onOptionsChange({
-      ...options,
-      secureJsonFields: {
-        ...options.secureJsonFields,
-        apiKey: false,
-      },
-      secureJsonData: {
-        ...options.secureJsonData,
-        apiKey: '',
-      },
-    });
-  };
-
   render() {
     const { options } = this.props;
-    const { jsonData, secureJsonFields } = options;
-    const secureJsonData = (options.secureJsonData || {}) as MySecureJsonData;
-
+    const { jsonData } = options;
     return (
       <div className="gf-form-group">
         <div className="gf-form">
           <FormField
-            label="Path"
+            label="URL"
             labelWidth={6}
-            inputWidth={20}
+            inputWidth={30}
             onChange={this.onPathChange}
-            value={jsonData.path || ''}
-            placeholder="json field returned to frontend"
+            value={jsonData.url || ''}
+            placeholder="Solr URL (https://solr.com:8983/solr)"
           />
         </div>
-
-        <div className="gf-form-inline">
-          <div className="gf-form">
-            <SecretFormField
-              isConfigured={(secureJsonFields && secureJsonFields.apiKey) as boolean}
-              value={secureJsonData.apiKey || ''}
-              label="API Key"
-              placeholder="secure json field (backend only)"
-              labelWidth={6}
-              inputWidth={20}
-              onReset={this.onResetAPIKey}
-              onChange={this.onAPIKeyChange}
-            />
-          </div>
+        <div className="gf-form">
+          <FormField
+            label="Collection"
+            labelWidth={6}
+            inputWidth={30}
+            onChange={this.onPathChange}
+            value={jsonData.collection || ''}
+            placeholder="json field returned to frontend"
+          />
         </div>
       </div>
     );
